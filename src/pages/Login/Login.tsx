476b711aab4a2f5ceb645649e5ce55865ad8f9c0
cole-rose <coletherose@gmail.com>
2024-05-09 10:30:46 -0400

    Setup gitignore, setup basic project structure with routes, setup material ui, need to create hooks

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+// import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+export const Login = () => {
+  return <Container>Login</Container>;
+};
